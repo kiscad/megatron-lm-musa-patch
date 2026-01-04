608c0f81a4c5e9cbe1f0faabffe367ee79aa5b8f
Zhihui Zheng(MCC) <zhihui.zheng@mthreads.com>
2026-01-04 15:17:40 +0800

    add support for megatron local ckpt

--- musa_patch/checkpointing.py
+++ musa_patch/checkpointing.py
@@ -1,76 +1,21 @@
+import logging
 import os
 
-from megatron.training.global_vars import (
-    get_args,
+from megatron.core.dist_checkpointing.mapping import (
+    CommonStateDict,
 )
 
-def save_checkpoint(iteration, model, optimizer, opt_param_scheduler,
-                    num_floating_point_operations_so_far,
-                    checkpointing_context=None, pipeline_rank=None,
-                    expert_rank=None, tensor_rank=None,
-                    pipeline_parallel=None, expert_parallel=None,
-                    non_persistent_ckpt=False,
-                    train_data_iterator=None, ft_client=None,
-                    preprocess_common_state_dict_fn=None
-                    ):
-  try:
-    from dlrover.trainer.torch.flash_checkpoint.megatron_dist_ckpt \
-      import save_checkpoint as dlrover_save_checkpoint_dist
-    from dlrover.trainer.torch.flash_checkpoint.megatron \
-      import save_checkpoint as dlrover_save_checkpoint
-  except Exception as e:
-    print(f"import flash_ckpt failed {str(e)}")
-    return
+logger = logging.getLogger(__name__)
 
-  args = get_args()
-  if args.use_distributed_optimizer and not args.no_save_optim:
-    dlrover_save_checkpoint_dist(iteration, model, optimizer,
-                                 opt_param_scheduler, 0,
-                                 preprocess_common_state_dict_fn)
-  else:
-    dlrover_save_checkpoint(iteration, model, optimizer,
-                            opt_param_scheduler, 0)
+# The common state dict is intended to represent non-tensor states to be persisted in the checkpoint.
+# This validation check is run in order to warn users if there is a difference across ranks, as only
+# the common state dict from global rank 0 is saved and non-rank-zero common states are not persisted.
+# local type不许要校验，每个rank都会保存common state dict
+def _validate_common_state_dict(common_state_dict: CommonStateDict) -> None:
+  pass
 
-def load_checkpoint(model, optimizer, opt_param_scheduler,
-                    load_arg='load', strict=True,
-                    ft_client=None, checkpointing_context=None,
-                    skip_load_to_model_and_opt=False):
-  try:
-    from dlrover.trainer.torch.flash_checkpoint.megatron_dist_ckpt \
-      import load_checkpoint as dlrover_load_checkpoint_dist
-    from dlrover.trainer.torch.flash_checkpoint.megatron \
-      import load_checkpoint as dlrover_load_checkpoint
-  except Exception as e:
-    print(f"import flash_ckpt failed {str(e)}")
-    return 0, 0
-
-  i = 0
-  args = get_args()
-  if args.use_distributed_optimizer and not args.no_save_optim:
-    i,  num_floating_point_operations_so_far = dlrover_load_checkpoint_dist(model,
-                                        optimizer,
-                                        opt_param_scheduler,
-                                        load_arg,
-                                        strict)
-  else:
-    i, num_floating_point_operations_so_far = dlrover_load_checkpoint(model,
-                                optimizer,
-                                opt_param_scheduler,
-                                load_arg,
-                                strict,
-                                ft_client=ft_client,
-                                checkpointing_context=checkpointing_context,
-                                skip_load_to_model_and_opt=skip_load_to_model_and_opt)
-
-  return i, num_floating_point_operations_so_far
-
-enable_async_ckpt = int(os.getenv("ENABLE_ASYNC_CKPT", 0))
+enable_async_ckpt = os.getenv("CKPT_DIR_PATH", "")
 if enable_async_ckpt:
   print("flash ckpt enabled")
-  import megatron.training.checkpointing
-
-  megatron.training.checkpointing.save_checkpoint = save_checkpoint
-  megatron.training.checkpointing.load_checkpoint = load_checkpoint
-
-  megatron.training.training.save_checkpoint = save_checkpoint
-  megatron.training.training.load_checkpoint = load_checkpoint
+  import megatron.core.dist_checkpointing.validation
+  megatron.core.dist_checkpointing.validation._validate_common_state_dict = _validate_common_state_dict
--- musa_patch/training.py
+++ musa_patch/training.py
@@ -801,7 +801,7 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                 args.consumed_train_samples += batch_size
                 args.skipped_train_samples += batch_size
                 continue
-                
+
             args.curr_iteration = iteration
             loss_dict, skipped_iter, should_checkpoint, should_exit, exit_code, grad_norm, num_zeros_in_grad = \
                 train_step(forward_step_func,
@@ -810,7 +810,7 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
                         optimizer,
                         opt_param_scheduler,
                         config)
-            
+
             moe_monitor.step()
 
             if int(os.getenv("USE_EPX", "0")):
@@ -989,7 +989,9 @@ def train(forward_step_func, model, optimizer, opt_param_scheduler,
     if args.enable_ft_package and ft_integration.get_rank_monitor_client() is not None:
         ft_integration.get_rank_monitor_client().shutdown_workload_monitoring()
 
-    maybe_finalize_async_save(blocking=True)
+    # This will finalize all unfinalized async request and terminate
+    # a persistent async worker if persistent ckpt worker is enabled
+    maybe_finalize_async_save(blocking=True, terminate=(should_exit is True))
 
     # If any exit conditions (signal handler, duration, iterations) have been reached, exit.
     if should_exit:
